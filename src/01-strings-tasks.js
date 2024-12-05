b21eb1e8009ed1a834155b5cc4cf482fd95af514
Ivan Ostrovsky <ostrovskyiml@gmail.com>
2024-12-05 10:59:22 +0300

    created_02_numbers(editing_01)

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -1,47 +1,125 @@
+/**
+ * Returns the result of concatenation of two strings.
+ *
+ * @param {string} value1
+ * @param {string} value2
+ * @return {string}
+ */
 function concatenateStrings(value1, value2) {
   return value1 + value2;
 }
 
+/**
+ * Returns the length of given string.
+ *
+ * @param {string} value
+ * @return {number}
+ */
 function getStringLength(value) {
   return value.length;
 }
 
+/**
+ * Returns the result of string template and given parameters firstName and lastName.
+ *
+ * @param {string} firstName
+ * @param {string} lastName
+ * @return {string}
+ */
 function getStringFromTemplate(firstName, lastName) {
   return `Hello, ${firstName} ${lastName}!`;
 }
 
+/**
+ * Extracts a name from template string 'Hello, First_Name Last_Name!'.
+ *
+ * @param {string} value
+ * @return {string}
+ */
 function extractNameFromTemplate(value) {
   return value.slice(7, -1);
 }
 
+/**
+ * Returns a first char of the given string.
+ *
+ * @param {string} value
+ * @return {string}
+ */
 function getFirstChar(value) {
   return value[0];
 }
 
+/**
+ * Removes a leading and trailing whitespace characters from string.
+ *
+ * @param {string} value
+ * @return {string}
+ */
 function removeLeadingAndTrailingWhitespaces(value) {
   return value.trim();
 }
 
+/**
+ * Returns a string that repeated the specified number of times.
+ *
+ * @param {string} value
+ * @param {number} count
+ * @return {string}
+ */
 function repeatString(value, count) {
   return value.repeat(count);
 }
 
+/**
+ * Remove the first occurrence of string inside another string
+ *
+ * @param {string} str
+ * @param {string} value
+ * @return {string}
+ */
 function removeFirstOccurrences(str, value) {
   return str.replace(value, '');
 }
 
+/**
+ * Remove the first and last angle brackets from tag string
+ *
+ * @param {string} str
+ * @return {string}
+ */
 function unbracketTag(str) {
   return str.slice(1, -1);
 }
 
+/**
+ * Converts all characters of the specified string into the upper case
+ *
+ * @param {string} str
+ * @return {string}
+ */
 function convertToUpperCase(str) {
   return str.toUpperCase();
 }
 
+/**
+ * Extracts e-mails from single string with e-mails list delimited by semicolons
+ *
+ * @param {string} str
+ * @return {array}
+ */
 function extractEmails(str) {
   return str.split(';');
 }
 
+/**
+ * Returns the string representation of rectangle with specified width and height
+ * using pseudographic chars
+ *
+ * @param {number} width
+ * @param {number} height
+ * @return {string}
+ */
 function getRectangleString(width, height) {
   const top = `┌${'─'.repeat(width - 2)}┐\n`;
   const middle = `│${' '.repeat(width - 2)}│\n`;
@@ -49,16 +127,40 @@ function getRectangleString(width, height) {
   return top + middle.repeat(height - 2) + bottom;
 }
 
+/**
+ * Encode specified string with ROT13 cipher
+ *
+ * @param {string} str
+ * @return {string}
+ */
 function encodeToRot13(str) {
-  return str.replace(/[A-Za-z]/g, (char) => String.fromCharCode(
-    char.charCodeAt(0) + (char.toLowerCase() < 'n' ? 13 : -13),
+  return str.replace(/[A-Za-z]/g, (char) => (
+    String.fromCharCode(
+      char.charCodeAt(0) + (char.toLowerCase() < 'n' ? 13 : -13),
+    )
   ));
 }
-
+/**
+ * Returns true if the value is string; otherwise false.
+ * @param {string} value
+ * @return {boolean}
+ */
 function isString(value) {
   return typeof value === 'string' || value instanceof String;
 }
 
+/**
+ * Returns playing card id.
+ *
+ * Playing cards initial deck includes the cards in the following order:
+ * 'A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣',
+ * 'A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦',
+ * 'A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥',
+ * 'A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠'
+ *
+ * @param {string} value
+ * @return {number}
+ */
 function getCardId(value) {
   const suits = ['♣', '♦', '♥', '♠'];
   const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
